e4f15e0a5541606213330c1feba6d249ba3f3181
Owen Holloway <owen.holloway101@gmail.com>
2014-01-16 20:21:34 +1100

    I am done for today

--- phptest/index.php
+++ phptest/index.php
@@ -1,3 +1,14 @@
 <?php
+
+//Connect to the DB!
 require "db/connect.php";
+
+//Connects to the DB and queries             if query fails it outputs error and dies
+$result = $db->query("SELECT * FROM people") or die($db->error);
+
+//check that the DB has got rows, if not something funky is happening
+if ($result->num_rows) {
+	
+}
+
 ?>
