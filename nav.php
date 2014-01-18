390429a090a908cb5166887ab542331a81386e0f
Owen Holloway <owen.holloway101@gmail.com>
2014-01-18 22:58:38 +1100

    the nav bar is now finsihed .... if unpopulated

--- nav.php
+++ nav.php
@@ -1,4 +1,12 @@
-<link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+<!--<link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>-->
 <head>
 	<link rel="stylesheet" type="text/css" href="mainStyles.css">
+	<link rel="shortcut icon" href="/assets/zeryt.ico" >
 </head>
+
+<body>
+	<img class="logo" src="/assets/zerytLogo.png">
+	<div class="nav">
+		This is test text
+	</div>
+</body>
