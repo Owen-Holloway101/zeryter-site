787e5243ba21f60335fe5e8a36f97e3fac3e79da
Owen Holloway <owen.holloway101@gmail.com>
2014-01-19 22:35:09 +1100

    doing some db stuff

---
+++ db/connect.php
@@ -0,0 +1,18 @@
+<?php
+//error reporting on/off 
+/*
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+*/
+//inits the variable $db with the connection to the server
+include("dblogin.private");
+$db = setDB();
+
+if($db->connect_errno > 0) {
+        die("DB fail <br>");
+} else {
+        echo("DB connected <br>");
+}
+
+?>
--- nav.php
+++ nav.php
@@ -7,9 +7,9 @@
 <body>
 	<img class="logo" src="/assets/zerytLogo.png">
 	<div class="nav">
-		<button onclick="location.href='index.php'" class="zerytButton">HOME</button>
-		<button onclick="location.href='about.php'" class="zerytButton">ABOUT</button>
-		<button onclick="location.href='thoughtStream.php'"class="zerytButton">THOUGHT STREAM</button>
+		<button onclick="location.href='/'" class="zerytButton">HOME</button>
+		<button onclick="location.href='/about.php'" class="zerytButton">ABOUT</button>
+		<button onclick="location.href='/thoughtStream.php'"class="zerytButton">THOUGHT STREAM</button>
 		Zeryter: that one guy that does stuff on the internet
 	</div>
 </body>
