c02a55276e2023ff055a0a2bde6f330980cfde5e
Owen Holloway <owen.holloway101@gmail.com>
2014-01-20 23:15:04 +1100

    working on the backend

--- loginUser.php
+++ loginUser.php
@@ -6,13 +6,22 @@ require 'db/userConnect.php';
 
 <head>
 	<title>Login User</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript">
+	function login () {
+		var user = $("input#username").val();
+		console.log(user);
+		var pass = $("input#password").val();
+		console.log(pass);
+	}
+	</script>
 </head>
 
 <body>
 	<div class="content">
-		<input class="zerytInput" /*           */ name="user" placeholder="Username" /* */></input><br>
-		<input class="zerytInput" type="password" name="pass" placeholder="Password" /* */></input><br>
-		<span class="zerytButtonFlat" style="display: inline-block; magin-left: 10px;">LOGIN</span>
+		<input class="zerytInput" /*           */ name="user" placeholder="Username" id="username"></input><br>
+		<input class="zerytInput" type="password" name="pass" placeholder="Password" id="password"></input><br>
+		<span  class="zerytButtonFlat" style="display: inline-block; magin-left: 10px;" onClick=login()>Login</span>
 	</div>
 </body>
 
--- nav.php
+++ nav.php
@@ -7,9 +7,9 @@
 <body>
 	<img class="logo" onclick="location.href='/loginUser.php'" src="/assets/zerytLogo.png">
 	<div class="nav">
-		<span onclick="location.href='/'" class="zerytButtonFlat">HOME</span>
-		<span onclick="location.href='/about.php'" class="zerytButtonFlat">ABOUT</span>
-		<span onclick="location.href='/thoughtStream.php'"class="zerytButtonFlat">THOUGHT STREAM</span>
+		<span onclick="location.href='/'" class="zerytButtonFlat">Home</span>
+		<span onclick="location.href='/about.php'" class="zerytButtonFlat">About</span>
+		<span onclick="location.href='/thoughtStream.php'"class="zerytButtonFlat">Thought Stream</span>
 		<t> &#160;&#160;&#160;&#160;&#160;&#160;&#160; Zeryter: that one guy that does stuff on the internet</t>
 	</div>
 </body>
