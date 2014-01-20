03d313bf6fb938a7072f549a998feaf847c61a26
Owen Holloway <owen.holloway101@gmail.com>
2014-01-20 19:32:29 +1100

    move the db connection statement for better continuity

---
+++ dbTests.php
@@ -0,0 +1,27 @@
+<?php
+
+include "db/connect.php";
+
+$query = "SELECT UNAME, SPASS, SALT FROM USERS";
+
+if ($stmt = $db->prepare($query)) {
+
+    /* execute statement */
+    $stmt->execute();
+
+    /* bind result variables */
+    $stmt->bind_result($uname, $spass, $salt);
+
+    /* fetch values */
+    while ($stmt->fetch()) {
+    	if ($uname == "asdffd") {
+    		printf ("%s %s %s <br>", $uname, $spass, $salt);
+
+    	}
+    }
+
+    /* close statement */
+    $stmt->close();
+}
+
+?>
--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <!--<link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>-->
 <head>
-	<link rel="stylesheet" type="text/css" href="mainStyles.css">
+	<link rel="stylesheet" type="text/css" href="/mainStyles.css">
 	<link rel="shortcut icon" href="/assets/zeryt.ico" >
 </head>
 
