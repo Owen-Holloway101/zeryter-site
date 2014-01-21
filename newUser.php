1aa3bac0478f52e0c0918ca553752473d390a707
Owen Holloway <owen.holloway101@gmail.com>
2014-01-21 14:17:37 +1100

    made some checks for password

--- newUser.php
+++ newUser.php
@@ -10,14 +10,17 @@ require 'db/userConnect.php';
 	<script type="text/javascript">
 	function createUser () {
 		var user = $("input#username").val();
-		console.log(user);
 		var pass = $("input#password").val();
-		console.log(pass);
-		var expDate = new Date();
-		expDate.setTime(expDate.getTime()+(60*60)); //set to an hour from Now
-		document.cookie="newUser=" + user + ";" + expDate;
-		document.cookie="newPass=" + pass + ";" + expDate;
-		window.location.href="createUser.php"
+		var passCheck = $("input#passwordCheck").val();
+		if (pass == passCheck) {
+			var expDate = new Date();
+			expDate.setTime(expDate.getTime()+(60*60)); //set to an hour from Now
+			document.cookie="newUser=" + user + ";" + expDate;
+			document.cookie="newPass=" + pass + ";" + expDate;
+			window.location.href="createUser.php";
+		} else {
+			$("#passCheckOut").text("Passwords need to match");
+		}
 	}
 	</script>
 </head>
@@ -34,7 +37,7 @@ require 'db/userConnect.php';
 		echo '
 		<input class="zerytInput" /*           */ name="user" placeholder="Username" /* */ id="username" /**/></input><br>
 		<input class="zerytInput" type="password" name="pass" placeholder="Password" /* */ id="password" /**/></input><br>
-		<input class="zerytInput" type="password" name="pass" placeholder="Password Again" id="passwordCheck"></input><br>
+		<input class="zerytInput" type="password" name="pass" placeholder="Password Again" id="passwordCheck"></input><span style="margin-left: 10px ;" id="passCheckOut"></span><br>
 		<span  class="zerytButtonFlat" style="display: inline-block; magin-left: 10px;" onClick=createUser()>Create User</span>
 		';
 	}
