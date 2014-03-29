28affad9243961835873606858cd013572264c87
Owen Holloway <owen.holloway101@gmail.com>
2014-03-29 17:54:33 +1100

    video!

--- test.php
+++ test.php
@@ -4,3 +4,9 @@ require 'nav.php';
 <head>
 	<title>test page</title>
 </head>
+
+<body>
+	<video height="auto" width="80%" controls>
+		<source src="assets/thing.mp4" type="video/mp4">
+	</video>
+</body>
