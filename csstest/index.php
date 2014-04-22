fbb83c833930002361fb746dc01cd7d584b55847
Owen <owen.holloway101@gmail.com>
2014-04-22 22:22:47 +1000

    I dun broke things ....

--- csstest/index.php
+++ csstest/index.php
@@ -1,11 +1,14 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="/csstest/mainStyles.css">
-	<link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 800px)" href="/csstest/mobileStyles.css" />
+	<link rel="stylesheet" type="text/css" media="screen only and (max-width: 800px)" href="/csstest/mobileStyles.css" />
 </head>
 
 <body>
 	<div class="nav">
+	<div class="overflow">
+	</div>
+	Header Text
 	</div>
 	<?php
 	for ($i=0; $i < 100; $i++) { 
